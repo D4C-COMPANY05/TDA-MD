c7775977a7ce018c57a1265d06effdd790bbc13e
D4C-COMPANY05 <d4c.company05@gmail.com>
2025-09-20 11:21:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,9 @@ module.exports = {
   NOM_OWNER: process.env.OWNER || "Kiyotaka Ayanokoji",
   MODE: process.env.MODE || "dev",
   PORT: process.env.PORT || 3000,
-  SOCKET_TOKEN: process.env.SOCKET_TOKEN || null
+  SOCKET_TOKEN: process.env.SOCKET_TOKEN || null,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY
 };
+
