dd19ec9f3ff6b1a175ae22dfa111ecb1683f7a08
D4C-COMPANY05 <d4c.company05@gmail.com>
2025-09-24 00:55:43 -0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,13 +1,11 @@
 const mega = require("megajs");
 const fs = require("fs");
 
-let email = 'd4c.company05@gmail.com'; // email du compte Mega
-let pw = 'Anne-2005!'; // mot de passe Mega
+let email = 'd4c.company05@gmail.com'; 
+let pw = 'Anne-2005!'; 
 
-// Fonction pour uploader un fichier local sur Mega
 async function upload(stream, filename) {
     return new Promise((resolve, reject) => {
-        // Se connecter au compte Mega
         const storage = new mega.Storage({
             email: email,
             password: pw
@@ -15,12 +13,11 @@ async function upload(stream, filename) {
 
         storage.on('ready', () => {
             console.log("✅ Mega storage ready. Starting upload...");
-            // Utilisez la méthode upload() de l'objet storage, qui prend le flux de données
             const uploadStream = storage.upload({ name: filename }, stream);
 
             uploadStream.on('complete', () => {
                 console.log(`✅ File uploaded successfully: ${filename}`);
-                resolve(uploadStream.file.link); // lien du fichier uploadé
+                resolve(uploadStream.file.link);
             });
 
             uploadStream.on('error', reject);
